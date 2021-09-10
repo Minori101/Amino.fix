4f0448576fa63ce29a6b45682ead96eed424fdc7
Minori <te3594424@gmail.com>
2021-09-10 20:07:32 +0300

    1.2.7

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
 	name="amino.fix",
 	license = 'MIT',
 	author="Minori",
-	version="1.2.5",
+	version="1.2.7",
 	author_email="",
 	description="Amino 1.2.17 fix lib. Discord - https://discord.gg/Bf3dpBRJHj",
 	url="https://github.com/Minori100/Amino.fix",
