3561e7bcb23ee18877f9ab6943aa434f77bd636b
Minori <te3594424@gmail.com>
2021-11-06 21:35:02 +0300

    2.0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 requirements = ["aiohttp", "requests", "asyncio", "typing", "websocket-client==0.57.0", "setuptools", "json_minify",
-                "six"]
+                "six", "websockets"]
 
 with open("README.md", "r") as stream:
     long_description = stream.read()
@@ -10,7 +10,7 @@ setup(
     name="amino.fix",
     license='MIT',
     author="Minori",
-    version="2.0.1",
+    version="2.0.2",
     author_email="",
     description="Amino 1.2.17 fix lib. Discord - https://discord.gg/Bf3dpBRJHj",
     url="https://github.com/Minori100/Amino.fix",
