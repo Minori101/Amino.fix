b7941d37b5b41a4c01eb49dc2b5a91e5f2f6492d
Minori101 <96193885+Minori101@users.noreply.github.com>
2021-12-30 18:23:09 +0300

    2.1.7

--- aminofix/lib/util/helpers.py
+++ aminofix/lib/util/helpers.py
@@ -7,14 +7,24 @@ from functools import reduce
 from base64 import b64decode
 from typing import Union
 import requests
+import random
+import string
 
 def generate_device_info() -> dict:
+
+    data = "".join(random.choices(string.ascii_uppercase + string.ascii_lowercase + "_-", k=462)).replace("--", "-")
+    device = deviceId(data)
+
     return {
-        "device_id": "32E3DDD1C3607790DE1D2F4AECD1C6E81D3F49EB2B93DC1203984EE5B83EAEA89B80A2D32978ABFD1A",
+        "device_id": device,
         "user_agent": "Dalvik/2.1.0 (Linux; U; Android 7.1.2; SM-G965N Build/star2ltexx-user 7.1.; com.narvii.amino.master/3.4.33592)"
     }
 
 # okok says: please use return annotations :(( https://www.python.org/dev/peps/pep-3107/#return-values
+
+def deviceId(data):
+    uids = str(data)
+    return ("32" + sha1(uids.encode()).digest().hex() + hmac.new(bytes.fromhex("76b4a156aaccade137b8b1e77b435a81971fbd3e"), b'\x32' + sha1(uids.encode()).digest(), sha1).hexdigest()).upper()
 
 def signature(data: Union[str, dict]) -> str:
     if isinstance(data, dict): data = json.dumps(data)
