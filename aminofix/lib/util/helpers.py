3d48222205e0e0e0fa9a82f941d14610adb40703
Alert Aigul <alertaigul@gmail.com>
2022-04-16 17:40:12 +0300

    Added signature and deviceId native generators.

--- aminofix/lib/util/helpers.py
+++ aminofix/lib/util/helpers.py
@@ -1,11 +1,15 @@
 import json
+import os
 
 from functools import reduce
-from base64 import b64decode
+from base64 import b64decode, b64encode
 from typing import Union
-import requests
+from hashlib import sha1
+from hmac import new
 
-session = requests.Session()
+PREFIX = bytes.fromhex("42")
+SIG_KEY = bytes.fromhex("F8E7A61AC3F725941E3AC7CAE2D688BE97F30B93")
+DEVICE_KEY = bytes.fromhex("02B258C63559D8804321C5D5065AF320358D366F")
 
 def generate_device_info() -> dict:
 
@@ -14,21 +18,17 @@ def generate_device_info() -> dict:
         "user_agent": "Dalvik/2.1.0 (Linux; U; Android 7.1.2; SM-G965N Build/star2ltexx-user 7.1.; com.narvii.amino.master/3.4.33602)"
     }
 
-def deviceId(data: str = None) -> str:
-    if data == None:
-        response = session.get(f"https://ed-generators.herokuapp.com/device")
-    else:
-        response = session.get(f"https://ed-generators.herokuapp.com/device?data={data}")
-    return response.text.upper()
+def deviceId(data: bytes = None) -> str:
+    identifier = PREFIX + data or os.urandom(20)
+    mac = new(DEVICE_KEY, identifier, sha1)
+    return f"{identifier.hex()}{mac.hexdigest()}".upper()
 
-#@dorthegra/IDörthe#8835 the server was bought with his money :p you can write to him to thank you 
-def signature(data: Union[str, dict]) -> str:
-    response = session.post("https://ed-generators.herokuapp.com/signature", data=data)
-    return response.text
+def signature(data: Union[str, bytes]) -> str:
+    data = data if isinstance(data, bytes) else data.encode("utf-8")
+    return b64encode(PREFIX + new(SIG_KEY, data, sha1).digest()).decode("utf-8")
 
-def update_deviceId(deviceId: str) -> str:
-    response = session.get(f"https://ed-generators.herokuapp.com/update-device?device={deviceId}")
-    return response.text
+def update_deviceId(device: str) -> str:
+    return deviceId(bytes.fromhex(device[2:42]))
 
 def decode_sid(sid: str) -> dict:
     return json.loads(b64decode(reduce(lambda a, e: a.replace(*e), ("-+", "_/"), sid + "=" * (-len(sid) % 4)).encode())[1:-20].decode())
