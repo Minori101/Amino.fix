a47b1e2ca221d1d754d2a7f398733d7c5806f231
Minori <96193885+Minori101@users.noreply.github.com>
2023-04-06 21:04:49 +0300

    2.3.5.5

--- aminofix/__init__.py
+++ aminofix/__init__.py
@@ -2,7 +2,7 @@ __title__ = 'Amino.fix'
 __author__ = 'Minori'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2021-2023 Minori'
-__version__ = '2.3.5.4'
+__version__ = '2.3.5.5'
 
 from .acm import ACM
 from .client import Client
