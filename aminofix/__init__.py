f34b2a051a7f3c898911b1ccabbcd02ef4a4c7e8
Viktor <99266266+ViktorSky@users.noreply.github.com>
2023-06-26 21:29:47 -0400

    Update __init__.py

--- aminofix/__init__.py
+++ aminofix/__init__.py
@@ -10,15 +10,15 @@ from .sub_client import SubClient
 from .lib.util import exceptions, helpers, objects, headers
 from .asyncfix import acm, client, sub_client, socket
 from .socket import Callbacks, SocketHandler
-from requests import get
+from requests import ConnectionError, get
 from json import loads
 
 try:
     __newest__ = loads(get("https://pypi.org/pypi/amino.fix/json").text)["info"]["version"]
-except Exception:
+except ConnectionError:
     __newest__ = __version__
 finally:
-    del get, loads
+    del get, loads, ConnectionError
 
 if __version__ != __newest__:
     print(f"New version of {__title__} available: {__newest__} (Using {__version__})")
